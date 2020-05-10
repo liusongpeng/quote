d390508ac60bd3013427185b403ededca9ee482f
liusongpeng <13916688528@139.com>
2020-05-10 10:51:59 +0800

    change v3.3.4

--- quote.go
+++ quote.go
@@ -5,7 +5,7 @@
 // Package quote collects pithy sayings.
 package quote // import "rsc.io/quote"
 
-import "rsc.io/quote/v3"
+import quote "github.com/liusongpeng/quote/vlocal/v3"
 
 // Hello returns a greeting.
 func Hello() string {
